505936c52b3734b6b7b761502beede9275c7c573
nuredinbedruyimer <nuredinbedru06@gmail.com>
2025-09-25 19:07:48 -0700

    Set up Tailwind Css React-Router-Dom Done

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+        <h1>Hello React</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
