c933edd1064ffa49f2e713682eb7d6cfe94c44a6
nuredinbedruyimer <nuredinbedru06@gmail.com>
2025-09-25 22:17:44 -0700

    Add To cart , Remove From Cart Done

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,21 @@ import Searchbar from "./components/searchbar/Searchbar";
 import HomePage from "./pages/HomePage";
 import ProductsPage from "./pages/ProductsPage";
 import ProductDetailsPage from "./pages/ProductDetailsPage";
+import CartPage from "./pages/CartPage";
+import { ToastContainer, toast } from "react-toastify";
 
 function App() {
   return (
     <>
       <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
         <Navbar/>
+        <ToastContainer/>
         <Searchbar/>
         <Routes>
            <Route path="/" element={<HomePage />} />
            <Route path="/products" element={<ProductsPage />} />
            <Route path="/product/:productId" element={<ProductDetailsPage />} />
+           <Route path="/cart" element={<CartPage />} />
         </Routes>
         
       </div>
